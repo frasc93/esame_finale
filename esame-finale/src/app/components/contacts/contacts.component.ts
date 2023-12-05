7f9a8b2d09114eca28d1502197485bc06b596529
frasc93 <scollofrancesca93@gmail.com>
2023-12-05 14:31:23 +0100

    add finals

--- esame-finale/src/app/components/contacts/contacts.component.ts
+++ esame-finale/src/app/components/contacts/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductsService } from 'src/app/shared/services/products.service';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-contacts',
@@ -10,12 +11,16 @@ export class ContactsComponent {
 
   products: any = [];
   
-  constructor ( private productService: ProductsService) {}
+  constructor ( private productService: ProductsService, private location: Location,) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(products => {
       this.products = products;
     });
+  }
+
+  navigateTo() {
+    this.location.back();
   }
 
 }
